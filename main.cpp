f7ee7409821f3b5c59c62c5fadd595cd8587caa1
i5heu <i5heu@i5h.eu>
2016-10-07 11:51:21 +0200

    updating the comments to english (from german)

--- main.cpp
+++ main.cpp
@@ -117,7 +117,7 @@ int main(){
 	if ( tmp1 == "1" || tmp1 == "2" || tmp1 == "3" || tmp1 == "4" || tmp1 == "5" || tmp1 == "6" || tmp1 == "7" || tmp1 == "8" ){
 		string tmp;
 		tmp = choice(tmp1);
-		cout << "\033[95mBitte gebe den zu mesenden Befehl ein\n>>\033[0m";
+		cout << "\033[95mPlease enter the command you wont massure\n>>\033[0m";
 		
 		getline(cin,userinput);
 		
@@ -138,8 +138,8 @@ int main(){
 		strcpy(tmpp, tmp1.c_str());
 	}
 	
-	cout << "\033[95m\n\nBenchmark fuer:\n"<< tmpp << "\n\n"; //dont ask me about the BBenchmark think ... ITS WORKING
-	cout << "\nXXXXX Program Startet XXXXX\033[0m\n ";
+	cout << "\033[95m\n\nBenchmark for:\n"<< tmpp << "\n\n";
+	cout << "\nXXXXX program START XXXXX\033[0m\n ";
 	
 		// START CLOCK
 		high_resolution_clock::time_point t1 = high_resolution_clock::now();
@@ -158,9 +158,9 @@ int main(){
 		lastcomandwrite();
 	}
 	
-	cout << "\033[95m\nXXXXX Program  FERTIG XXXXX";
-    cout << "\n" << duration << " Microsekudnen\n"; 
-    cout << (duration/1000) << " Milisekunden\n" ;
-    cout << (duration/1000000) << " Sekunden\n\033[0m" ;
+	cout << "\033[95m\nXXXXX program  FINISH XXXXX";
+    cout << "\n" << duration << " microseconds\n"; 
+    cout << (duration/1000) << " miliseconds\n" ;
+    cout << (duration/1000000) << " seconds\n\033[0m" ;
     return 0;
 }
